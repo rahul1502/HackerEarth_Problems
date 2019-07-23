7b446403ec74bbcf1346b5c26da422de7b77e984
rahul1502 <rahulvala1502@gmail.com>
2019-07-22 21:45:48 -0700

    July Circuit 2019 Problem 4

--- july_circuit_2019_prob_4.py
+++ july_circuit_2019_prob_4.py
@@ -1,4 +1,4 @@
-import itertools
+from itertools import product
 
 input_str = input().split(' ')
 n = int(input_str[0])
@@ -9,12 +9,9 @@ skills = list(map(int, input().split(' ')))
 master_arr = []
 for i in skills:
 	master_arr.append([j for j in range(0,i+1)]) 
-print('master_arr '+ str(master_arr))
-com_list = list(itertools.product(*master_arr))
 
-print('com_list '+ str(com_list))
 xor_list = []
-for i in com_list:
+for i in product(*master_arr):
 	xor = 0
 	for j in i:
 		xor ^= j
@@ -22,4 +19,4 @@ for i in com_list:
 
 mod = (10 ** 9) + 7
 for i in range(m+1):
-	print(xor_list.count(i)%mod,end = ' ')
+	print(xor_list.count(i)%mod, end=' ')
